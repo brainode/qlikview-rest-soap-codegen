ce6ce570cf9aaa7fcb9ce628036ad8de27c06a6d
brainode <admin@brainode.com>
2019-08-04 18:31:09 +0300

    Init commit

---
+++ SOAPGenCode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+
+namespace SOAPGenCode
+{
+    class Program
+    {
+        static string InputXmlPath = @"..\..\Data\349.xml";
+        static void Main(string[] args)
+        {
+            XElement soapResponse = XElement.Load(InputXmlPath);
+            Console.WriteLine("Test");
+        }
+    }
+}
